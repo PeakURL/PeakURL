f2b736b1a2defc9d101ee3704230d28d8a7f283d
Abd Ur Rehman <dev@peakurl.org>
2026-09-12 12:40:05 +0100

    Update install.php
    - Rehome browser installer entrypoint from site/install.php to root install.php.
    - Update database error redirect to /database-error.php.

--- install.php
+++ install.php
@@ -76,7 +76,7 @@ if ( InstallState::NEEDS_SETUP === $install_state ) {
 }
 
 if ( InstallState::DATABASE_CONNECTION_ERROR === $install_state ) {
-	header( 'Location: ' . InstallScreen::format_url( $base_path, '/db-error.php' ) );
+	header( 'Location: ' . InstallScreen::format_url( $base_path, '/database-error.php' ) );
 	exit();
 }
 
